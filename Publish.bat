bc3da1709aba1600ed7b1246d6956ba250184ce0
Denis Demenko <Denis.Demenko@aspose.com>
2016-11-23 10:33:41 +0200

    Use SmartStoreNET via submodule

---
+++ Publish.bat
@@ -0,0 +1,36 @@
+FOR %%b in (
+       "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+       "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+       "%ProgramFiles%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" 
+
+       "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+       "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+       "%ProgramFiles%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+       
+       "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+       "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+       "%ProgramFiles%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" 
+    ) do (
+    if exist %%b ( 
+       call %%b x86
+       goto build
+    )
+)
+
+echo "Unable to detect suitable environment. Build may not succeed."
+
+:build
+
+@echo off
+cls
+echo Building SmartStore.NET...   											
+
+cd /D Src\SmartStoreNet
+msbuild SmartStoreNET.proj /p:DebugSymbols=false /p:DebugType=None /P:SlnName=SmartStoreNET /maxcpucount /t:deploy
+
+rmdir ..\..\Publish /s /q
+mkdir ..\..\Publish
+xcopy /Y /E build\Web ..\..\Publish
+rmdir build /s /q
+
+pause
